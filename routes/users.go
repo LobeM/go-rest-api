0e6c82880b19401e1a21c863bfde592ba463272d
Lobe <lo.musonda@pacra.org.zm>
2024-05-13 15:05:56 +0200

    generated JWT

--- routes/users.go
+++ routes/users.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"example.com/rest-api/models"
+	"example.com/rest-api/utils"
 	"github.com/gin-gonic/gin"
 )
 
@@ -44,5 +45,12 @@ func login(ctx *gin.Context) {
 		return
 	}
 
-	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!"})
+	token, err := utils.GenerateToken(user.Email, user.ID)
+
+	if err != nil {
+		ctx.JSON(http.StatusInternalServerError, gin.H{"message": "Could not generate token."})
+		return
+	}
+
+	ctx.JSON(http.StatusOK, gin.H{"message": "Login successful!", "token": token})
 }
